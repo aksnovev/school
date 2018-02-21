6699069f75dba2f548fafe5bc6eedd11f5369dcf
aksnovev <aksnovev@gmail.com>
2018-02-21 18:52:26 +0200

    adaptive menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-var years = prompt('Сколько вам лет?', "");
+menu.onclick = function myFunction () {
+	var x = document.getElementById("myTopnav");
 
-alert('Вам ' + years + ' лет!')
-
+	if(x.className ==="topnav") {
+		x.className += " responsive";
+	} else{
+		x.className ="topnav";
+	}
+}
